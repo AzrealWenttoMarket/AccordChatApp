da45e67da5b64526370e2de9862a19511369d815
SherwinBautista312 <85328365+SherwinBautista312@users.noreply.github.com>
2022-06-22 21:21:56 +1200

    Add files via upload
    shesh

--- app.py
+++ app.py
@@ -1,20 +1,232 @@
-from flask import Flask, render_template
+#Imports
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_socketio import SocketIO, emit
+from werkzeug.security import check_password_hash, generate_password_hash
+from helper import *
+from termcolor import colored
+#importing SQLITE3 for database
+import sqlite3
 
+conn = sqlite3.connect("db.sqlite3", check_same_thread=False)
+c = conn.cursor()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secretkey"
 Socketio = SocketIO(app)
 
-@app.route("/")
+#set of unique users
+users = set()
+
+# App Routes
+
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("message.html")
+    if not session.get("user_id"):
+        return render_template("login.html")
+    else:
+        joinedChats = c.execute(f"SELECT * FROM rooms, users, user_room WHERE users.user_id=:u_id AND user_room.user_id=:u_id AND rooms.room_id==user_room.room_id",{"u_id": session["user_id"]}).fetchall()
+        if request.args.get("search"):
+            results = c.execute(f"SELECT * FROM rooms WHERE name LIKE '%{request.args.get('search')}%'").fetchall()
+            return render_template("searched.html", results=results)
+        else:
+            return render_template("index.html",joinedChats=joinedChats)
+
+@app.route("/room/<string:room_id>", methods=["GET", "POST"])
+@login_required
+def room(room_id):
+    if request.method == "POST":
+        invite = request.form.get("invite").split(",")
+        
+        room = c.execute("SELECT * FROM rooms WHERE room_id=:r_id", {"r_id": room_id}).fetchall()
+
+        if room[0][3] != "private":
+            return "You can't explicitly invite people unless it's your own private room"
+
+        user = c.execute("SELECT * FROM user_Room WHERE room_id=:r_id AND user_id=:u_id AND role='owner'", {"r_id": room_id, "u_id": session["user_id"]}).fetchall()
+
+        if len(user) == 0:
+            return "You can't explicitly invite people unless it's your own private room"
+
+        for i in invite:
+            user_id = c.execute("SELECT * FROM users WHERE email=:val OR username=:val", {"val": i}).fetchall()
+
+            if len(user_id) == 0:
+                return f"{i} not found"
+
+            c.execute("INSERT INTO user_room (user_id, room_id, role) VALUES (:u_id, :r_id, 'user')", {"u_id": user_id[0][0], "r_id": room_id})
+            conn.commit()
+
+        return "successfully invited all people on your list!"
+
+    else:
+        private = False
+        room = c.execute("SELECT * FROM rooms WHERE room_id=:r_id", {"r_id": room_id}).fetchall()
+        chatName = c.execute("SELECT name FROM rooms WHERE room_id=:r_id", {"r_id": room_id}).fetchall()
+        joinedChats = c.execute(f"SELECT * FROM rooms, users, user_room WHERE users.user_id=:u_id AND user_room.user_id=:u_id AND rooms.room_id==user_room.room_id",{"u_id": session["user_id"]}).fetchall()
+
+        if len(room) == 0:
+            return "Room not found"
+
+        if room[0][3] == "private":
+            access = c.execute("SELECT * FROM user_room WHERE user_id=:u_id AND room_id=:r_id", {"u_id": session.get("user_id"), "r_id": room_id}).fetchall()
+
+            if len(access) == 0:
+                return "404"
+
+            private = True
+
+        if len(c.execute("SELECT * FROM user_room WHERE room_id=:r_id and user_id=:u_id", {"r_id": room_id, "u_id": session.get("user_id")}).fetchall()) == 0:
+            c.execute("INSERT INTO user_room (user_id, room_id, role) VALUES (:u_id, :r_id, 'user')", {"r_id": room_id, "u_id": session.get("user_id")})
+            conn.commit()
+            username = c.execute("SELECT username FROM users WHERE user_id=:id", {"id": session.get("user_id")}).fetchall()[0][0]
+            c.execute("INSERT INTO messages (message, author, room, timestamp) VALUES (:m, :a, :r, :t)", {"m": f"Welcome {username}!", "a": "Bot", "r": room_id, "t": timestamp()})
+            conn.commit()
+            print(colored(room_id, "red"))
+            Socketio.emit("new people joined", {"message": f"Welcome {username}!", "room": room_id}, broadcast=True)
+            # socketio.emit("show message", {"message": f"Welcome {username}!"}, broadcast=True)
+
+        messages = c.execute("SELECT * FROM messages WHERE room=:r_id", {"r_id": room_id}).fetchall()
+        users = c.execute("SELECT * FROM user_room WHERE room_id=:r_id", {"r_id": room_id}).fetchall()
+        usernames = []
+        for user in users:
+            username = c.execute("SELECT username FROM users WHERE user_id=:u_id", {"u_id": user[0]}).fetchall()[0][0]
+            usernames.append(username)
+        return render_template("message.html", messages=messages, private=private, room_id=room_id, users=usernames, joinedChats=joinedChats,chatName=chatName)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if the form is valid
+
+            if not request.form.get("email") or not request.form.get("password") or not request.form.get("confirmation") or not request.form.get("username"):
+                return "please fill out all fields"
+
+            if request.form.get("password") != request.form.get("confirmation"):
+                return "password confirmation doesn't match password"
+
+            # check if email exist in the database
+            exist = c.execute("SELECT * FROM users WHERE email=:email OR username=:username", {"email": request.form.get("email"), "username": request.form.get("username")}).fetchall()
+
+            if len(exist) != 0:
+                return "user already registered"
+
+            # hash the password
+            pwhash = generate_password_hash(request.form.get("password"), method="pbkdf2:sha256", salt_length=8)
+
+            # insert the row
+            c.execute("INSERT INTO users (email, password, username) VALUES (:email, :password, :username)", {"email": request.form.get("email"), "password": pwhash, "username": request.form.get("username")})
+            conn.commit()
+
+            # return success
+            return "registered successfully!"
+    else:
+        return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        # check the form is valid
+        if not request.form.get("username") or not request.form.get("password"):
+            return "please fill out all required fields"
+
+        # check if email exist in the database
+        user = c.execute("SELECT * FROM users WHERE email=:email OR username=:username", {"email": request.form.get("username"), "username": request.form.get("username")}).fetchall()
+
+        if len(user) != 1:
+            return "you didn't register"
+
+        # check the password is same to password hash
+        pwhash = user[0][3]
+        if check_password_hash(pwhash, request.form.get("password")) == False:
+            return "wrong password"
+
+        # login the user using session
+        session["user_id"] = user[0][0]
+
+        # return success
+        return redirect("/")
+
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
+
+
+@app.route("/create-room", methods=["GET", "POST"])
+@login_required
+def create_room():
+    if request.method == "POST":
+        while True:
+            room_id = random_str()
+            rooms = c.execute("SELECT * FROM rooms WHERE room_id=:r_id", {"r_id": room_id}).fetchall()
+            if len(rooms) == 0:
+                c.execute("INSERT INTO rooms (room_id, name, description, status) VALUES (:r_id, :name, :desc, :status)", {"r_id": room_id, "name": request.form.get("name"), "desc": request.form.get("description"), "status": request.form.get('status')})
+                conn.commit()
+                break
+        c.execute("INSERT INTO user_room (user_id, room_id, role) VALUES (:u_id, :r_id, 'owner')", {"r_id": room_id, "u_id": session.get("user_id")})
+        conn.commit()
+        username = c.execute("SELECT username FROM users WHERE user_id=:id", {"id": session.get("user_id")}).fetchall()[0][0]
+        c.execute("INSERT INTO messages (message, author, room, timestamp) VALUES (:m, :a, :r, :t)", {"m": f"Welcome {username}!", "a": "Bot", "r": room_id, "t": timestamp()})
+        conn.commit()
+        Socketio.emit("new people joined", {"message": f"Welcome {username}!", "room": room_id}, broadcast=True)
+        return redirect(f"/room/{room_id}")
+    else:
+        return render_template("create-room.html")
+
+@app.route("/api")
+def api():
+    return jsonify(users=list(users))
+
+@app.route("/dm", methods=["GET"])
+@login_required
+def dm():
+    messages = c.execute("SELECT * FROM messages WHERE room=:room", {"room": f"user-{session.get('user_id')}"}).fetchall()
+    username = c.execute("SELECT username FROM users WHERE user_id=:u_id", {"u_id": session["user_id"]}).fetchall()[0][0]
+
+    return render_template("dm.html", messages=messages, username=username)
+
+# Event Listeners
 
 @Socketio.on("broadcast message")
 def message_display(data):
-    #call this event in the client side and display the console.log data.message
-    emit("show message", {"message": data["message"]},
-    broadcast=True)
+    ts = timestamp()
+    if session.get("user_id"):
+        user = c.execute("SELECT username FROM users WHERE user_id=:id", {"id": session["user_id"]}).fetchall()[0][0]
+    else:
+        user = "unknown"
+    c.execute("INSERT INTO messages (message, author, room, timestamp) VALUES (:m, :a, :r, :t)", {"m": data["message"], "a": user, "r": data["room_id"], "t": ts})
+    conn.commit()
+    emit("show message", {"message": data["message"],"timestamp": ts, "name": user, "room_id": data["room_id"]}, broadcast=True)
+
+@Socketio.on("disconnect")
+def exist():
+    print(session.get("user_id"), "disconnected!")
+    username = c.execute("SELECT username FROM users WHERE user_id=:id", {"id": session["user_id"]}).fetchall()[0][0]
+    users.discard(username)
+    emit("update status", {"user": username, "status": "offline"}, broadcast=True)
+
+@Socketio.on("connect")
+def connect():
+    print(str(session.get("user_id")) + " connected!")
+    username = c.execute("SELECT username FROM users WHERE user_id=:id", {"id": session["user_id"]}).fetchall()[0][0]
+    users.add(username)
+    emit("update status", {"user": username, "status": "online"}, broadcast=True)
+
+@Socketio.on("dm")
+def dm_socket(data):
+    ts = timestamp()
+    user_id = c.execute("SELECT * FROM users WHERE username=:name", {"name": data["username"]}).fetchall()[0][0]
+
+    c.execute("INSERT INTO messages (author, message, room, timestamp) VALUES (:a, :m, :r, :t)", {"a": data["author"], "m": data["message"], "r": f"user-{user_id}", "t": ts})
+    conn.commit()
+
+    emit("broadcast dm", {"author": data["author"], "receiver": data["username"], "t": ts, "message": data["message"]})
 
 if __name__ == '__main__':
-    Socketio.run(app, host="0.0.0.0", port=2000, debug=True)
+    Socketio.run(app, debug=True)
--- static/message.js
+++ static/message.js
@@ -1,5 +1,28 @@
 //when you submit a form, leave it there and do not refresh
 window.onload = function(e) {
+    
+    fetch('http://127.0.0.1:5000/api', {
+        method: 'GET', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => response.json())
+      .then(data => {
+            console.log(data)
+            for(let i=0; i<data["users"].length; i++) {
+                console.log(data["users"][i])
+                try {
+                    document.getElementById(data["users"][i]).innerHTML = "online";
+                    document.getElementById(data["users"][i]).setAttribute("style", "color: green;")
+                } catch(err) {
+                    var span = document.createElement("SPAN");
+                    span.innerHTML = `<span id=${data["users"][i]} style="color: green;">online</span>`
+                    document.getElementById("users").appendChild(span);
+                }
+            }
+      })
+
     $("#messageForm").submit(function (e) {
         e.preventDefault();
     });
@@ -9,19 +32,50 @@ window.onload = function(e) {
     //get value of what user wants to send then pass it to back-end and the send it back to front-end
     // front-end => back-end => front-end
     socket.on("connect", () => {
-        var button = document.getElementById
-        ("sendMessageButton");
-        //after user click submit do following codes
+        var button = document.getElementById("sendMessageButton");
         button.onclick = () => {
-            var message = document.getElementById
-            ("message").value;
-            socket.emit("broadcast message", 
-            //creating dictionary
-            {"message": message});
+            var message = document.getElementById("message").value;
+            document.getElementById("message").value = " ";
+            var room_id = location.pathname.split("/")[location.pathname.split("/").length-1];
+            console.log(room_id)
+            socket.emit("broadcast message", {"message": message, "room_id": room_id});
         }
+        // socket.emit("connect")
     });
 
     socket.on("show message", data => {
-        console.log(data.message);
+        if (data["room_id"] == location.pathname.split("/")[location.pathname.split("/").length-1]) {
+            $("#messages").append(`<small>${data.timestamp}</small><h4>${data.name}:</h4><p>${data.message}</p>`);
+            window.scrollTo(0, document.getElementById("messages").scrollHeight);
+        }
     });
+
+    socket.on("update status", data => {
+        console.log("status updated")
+        if (data["status"] == "online") {
+            try {
+                document.getElementById(data["user"]).setAttribute("style", "color: green;");
+            } catch (err) {
+                var SPAN = document.createElement("SPAN");
+                // SPAN.setAttribute("id", data["user"]);
+                SPAN.innerHTML = `${data["user"]} <span id="${data['user']}" style="color: green;">online</span>`
+                document.getElementById("users").appendChild(SPAN);
+            }
+        } else {
+            document.getElementById(data["user"]).setAttribute("style", "color: gray;")
+        }
+        document.getElementById(data["user"]).innerHTML = data["status"];
+    });
+
+    socket.on("new people joined", data => {
+        console.log("hi")
+        var room_id = location.pathname.split("/")[location.pathname.split("/").length-1];
+        console.log(room_id)
+        if (room_id === data["room_id"]) {
+            $(".container").append(`<small>${data.timestamp}</small><h4>${data.name}:</h4><p>${data.message}</p>`);
+            window.scrollTo(0, document.getElementById("messages").scrollHeight);
+            window.location.reload(true);
+        }
+    })
+
 }
